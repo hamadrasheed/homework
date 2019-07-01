65114250898d0753d783ad3a00f6b639c19bf1a0
hamadrasheed <50006928+hamadrasheed@users.noreply.github.com>
2019-07-01 12:59:33 +0500

    Basic variable Php 1

---
+++ try1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<body>
+
+	<?php 
+
+		$txt = "Hello Hamad";
+		$x = 11;
+		$y = 22.08;
+
+		echo "The value of x is $x <br>";
+		echo "The value of y is ". $y ."<br>";
+		echo "The value in string is ". $txt . "<br>";
+
+		echo "<br>";
+
+		echo "Addition of x and y is here: <br>";
+		echo "x + y = ";
+		echo $x + $y ;
+
+		echo "<br>";
+
+		echo "<br>Subtraction of x and y is here: <br>" ;
+		echo "x - y = ";
+		echo $x - $y ;
+
+		echo "<br>";
+
+		echo "<br>Multiplication of x and y is here: <br>";
+		echo "x * y = ";
+		echo $x * $y ;
+
+		echo "<br>";
+
+		echo "<br>Division of x and y is here: <br>" ;
+		echo "x / y = ";
+		echo $x / $y ;
+
+		echo "<br>";
+		
+		echo "<br>Modulus of x and y is here: <br>" ;
+		echo "x % y = ";
+		echo $x % $y ;
+
+
+
+	?>
+	
+</body>
+</html>
